d250def29ae624a0052ce70b65fe3617215bf432
abhishekk1004 <abhishekkushwaha.np@gmail.com>
2025-07-27 11:03:13 +0545

    for postgre

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 
-DATABASE_URL = "postgresql://postgres:5432@localhost/chatdb"
+DATABASE_URL = "postgresql://postgres:abhi18@localhost/chatdb"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
