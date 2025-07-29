18f73b997c7c6d640a8e9890bbfb69aaf3d0e0ce
abhishekk1004 <abhishekkushwaha.np@gmail.com>
2025-07-29 20:37:09 +0545

    databse file

--- database.py
+++ database.py
@@ -1,11 +1,19 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.ext.declarative import declarative_base
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
 
 
 DATABASE_URL = "postgresql://postgres:abhi18@localhost/chatdb"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+
+
 Base = declarative_base()
 
 def get_db():
